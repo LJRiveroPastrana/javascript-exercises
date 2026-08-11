71ffae3e85676c3fd4e2fef30113a0fd9aa894e7
LJRiveroPastrana <ljriverop@gmail.com>
2026-08-10 19:03:21 -0500

    exercises 6 to 8 javascript

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const result = [];
+
+    array.forEach((item) => {
+        // If the item is not in our list of things to remove, keep it
+        if (!args.includes(item)) {
+            result.push(item);
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
