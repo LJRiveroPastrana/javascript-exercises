7ec727e1a51a802b5c5fb222ade5665cd2f1cab4
LJRiveroPastrana <ljriverop@gmail.com>
2026-08-12 15:47:00 -0500

    solution exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+   if (Number.isInteger(num1) && num1 > 0 && Number.isInteger(num2) && num1 > 0) {
+    let  numMin = Math.min(num1,num2)
+    let  numMax = Math.max(num1,num2)
+    for (let index = numMin; index <= numMax; index++) {
+         sum += index;
+       
+    }
+    return sum
+   }
+    return "ERROR"
 };
 
 // Do not edit below this line
