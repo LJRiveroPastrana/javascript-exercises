a6e6b50f345c64840b101994a29443e4f3c79568
LJRiveroPastrana <ljriverop@gmail.com>
2026-08-13 15:30:02 -0500

    solution exercise 11 javascript tempConversion functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsiusConvert = ((tempFahrenheit - 32)*5)/9;
+  return Number(tempCelsiusConvert.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheitConvert = ((tempCelsius * 9)/5) + 32;
+  return Number(tempFahrenheitConvert.toFixed(1))
 };
 
 // Do not edit below this line
